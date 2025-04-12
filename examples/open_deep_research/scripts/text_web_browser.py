b77d49af20baaebdee9b4a7853e8dbb5f5c6d59d
Kindi BALDE <kindi.balde@gmail.com>
2025-04-12 23:54:41 +0200

    Update text_web_browser.py
    GoogleSearch is not imported properly. Better serpapi.search(params) instead

--- examples/open_deep_research/scripts/text_web_browser.py
+++ examples/open_deep_research/scripts/text_web_browser.py
@@ -213,7 +213,8 @@ class SimpleTextBrowser:
         if filter_year is not None:
             params["tbs"] = f"cdr:1,cd_min:01/01/{filter_year},cd_max:12/31/{filter_year}"
 
-        search = GoogleSearch(params)
+        #search = GoogleSearch(params)
+        search = serpapi.search(params)
         results = search.get_dict()
         self.page_title = f"{query} - Search"
         if "organic_results" not in results.keys():
